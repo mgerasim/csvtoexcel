f749e2955857470dbe9815c0e263a2118497710c
Mikhail Gerasimov <mgerasim@inbox.ru>
2017-04-03 01:09:50 +1100

    Добавление функции конфертации

--- CsvToExcel/Program.cs
+++ CsvToExcel/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace CsvToExcel
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string path = Directory.GetCurrentDirectory();
+                string[] files = System.IO.Directory.GetFiles(path, "*.csv");
+                foreach (var file in files)
+                {
+                    Console.WriteLine("Укажите имя файла (*.xlsx) для сохранения " + file);
+                    string filenameExcel = Console.ReadLine();
+                    ConvertCsvToExcel(file, filenameExcel);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ConvertCsvToExcel(string filenameCsv, string filenameExcel)
+        {
+
         }
     }
 }
