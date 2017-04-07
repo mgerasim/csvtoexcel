0796fc5cba39d42b60268bb1f9ae14ef705d8189
Mikhail Gerasimov <Gerasimov@dvugms.khv.ru>
2017-04-07 16:36:48 +1000

    finale

--- CsvToExcel/Program.cs
+++ CsvToExcel/Program.cs
@@ -11,27 +11,48 @@ namespace CsvToExcel
 {
     class Program
     {
+        static string fileLog;
+        static Settings theSetting;
+
+        static void Log(string msg)
+        {
+            if (fileLog.Length > 0 && (theSetting != null && theSetting.DEBUG > 2))
+            {
+                File.AppendAllText(fileLog, msg);
+                File.AppendAllText(fileLog, "\r\n");
+            }
+        }
         static void Main(string[] args)
         {
             try
             {
-                Settings theSetting = new Settings();
-                theSetting.Load();
                 string path = Directory.GetCurrentDirectory();
+                fileLog = path + "\\csv-to-excel.log";
+                
+                theSetting = new Settings(fileLog);
+                theSetting.Load();
 
                 string filename = path + "\\" + theSetting.FILENAME;
                 if (File.Exists(filename) == false)
                 {
-                    throw new Exception("Отсутствует файл " + filename);
+                    Log("Not FileExists" );
+                    throw new Exception("Отсутствует файл " );
+                }
+                else {
+                    Log("FileExists Ok");
                 }
                 Console.WriteLine("Укажите имя файла (*.xlsx): ");
                 string filenameExcel = Console.ReadLine();
                 filenameExcel = path + "\\" + theSetting.PREFIX + filenameExcel + ".xlsx";
+                Log("Excel File Name" );
                 ConvertCsvToExcel(filename, filenameExcel, theSetting);
 
             }
             catch (Exception ex)
             {
+                Log(ex.Message);
+                Log(ex.Source);
+                Log(ex.StackTrace);
                 Console.WriteLine(ex.Message);
                 Console.ReadKey();
             }
@@ -40,9 +61,16 @@ namespace CsvToExcel
 
         static void ConvertCsvToExcel(string filenameCsv, string filenameExcel, Settings theSettings)
         {
-            if (DateTime.Now > (new DateTime(2017,05,05))) {
-                return;
-            }
+            Log("ConvertCsvToExcel");
+            //Log("DateTime Now");
+            //Log(DateTime.Now.ToShortDateString());
+            //if (DateTime.Now > (new DateTime(2017,05,05))) {
+            //    Log("Protect");
+            //    return;
+            //} else
+            //{
+            //    Log("Protect Go");
+            //}
             string csvFileName = filenameCsv;
             string excelFileName = filenameExcel;
 
@@ -51,52 +79,119 @@ namespace CsvToExcel
             bool firstRowIsHeader = false;
 
             var format = new ExcelTextFormat();
-            format.Delimiter = ';';
-            format.EOL = "\r";              // DEFAULT IS "\r\n";
-            // format.TextQualifier = '"';
+
+            Log("theSettings.DELIMITER");
+            Log(theSettings.DELIMITER);
+            string DELIMITER = theSettings.DELIMITER;
+            DELIMITER = DELIMITER.Trim(new char[] { '\'' });
+            try
+            {
+                format.Delimiter = DELIMITER[0];
+            }
+            catch
+            {
+                Log("error delimiter");
+                format.Delimiter = ';';
+            }
+            
+            format.EOL = "\n";              // DEFAULT IS "\r\n";
+            format.TextQualifier = '"';
 
             using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFileName)))
             {
+                Log("ExcelPackage");
                 ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(worksheetsName);
                 worksheet.Cells["A1"].LoadFromText(new FileInfo(csvFileName), format, OfficeOpenXml.Table.TableStyles.None, firstRowIsHeader);
+
+                var colCnt = worksheet.Dimension.End.Column;
 
                 worksheet.Row(1).Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
                 worksheet.Row(1).Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
                 worksheet.Row(1).Style.WrapText = true;
                 worksheet.Row(1).Style.ShrinkToFit = true;
-                worksheet.Row(1).Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
-                worksheet.Row(1).Style.Fill.BackgroundColor.SetColor(Color.FromArgb(
-                    Convert.ToInt32(theSettings.COLOR_FILL_HEADER.Split(new char[] {'-'}, StringSplitOptions.None)[0]), 
-                    Convert.ToInt32(theSettings.COLOR_FILL_HEADER.Split(new char[] {'-'}, StringSplitOptions.None)[1]), 
-                    Convert.ToInt32(theSettings.COLOR_FILL_HEADER.Split(new char[] {'-'}, StringSplitOptions.None)[2]) ));
+                worksheet.Cells[1, 1, 1, colCnt].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+
+                Log("COLOR_FILL_HEADER");
+                Log(theSettings.COLOR_FILL_HEADER);
+                foreach (var token in theSettings.COLOR_FILL_HEADER.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Log(token);
+                }
+
+                worksheet.Cells[1, 1, 1, colCnt].Style.Fill.BackgroundColor.SetColor(Color.FromArgb(
+                    Convert.ToInt32(theSettings.COLOR_FILL_HEADER.Split(new char[] {'-'}, StringSplitOptions.RemoveEmptyEntries)[0]), 
+                    Convert.ToInt32(theSettings.COLOR_FILL_HEADER.Split(new char[] {'-'}, StringSplitOptions.RemoveEmptyEntries)[1]), 
+                    Convert.ToInt32(theSettings.COLOR_FILL_HEADER.Split(new char[] {'-'}, StringSplitOptions.RemoveEmptyEntries)[2]) ));
 
                 worksheet.Cells[1, 2, 1, 2].Style.Font.Bold = true;
                 worksheet.Cells[1, 17, 1, 17].Style.Font.Bold = true;
 
                 for (int i = 1; i <= 27; i++)
                 {
-                    worksheet.Column(i).Width = 10;
+                    worksheet.Column(i).Width = (theSettings.SIZE_DEFAULT_IN_PX + 5 - 12) / 7d + 1; ;
                 }
 
-                worksheet.Column(2).Width = 25;
-                worksheet.Column(4).Width = 14;
-                worksheet.Column(9).Width = 8;
-                worksheet.Column(10).Width = 10;
-                worksheet.Column(11).Width = 8;
-                worksheet.Column(12).Width = 8;
-                worksheet.Column(13).Width = 8;
-                worksheet.Column(14).Width = 7;
-                worksheet.Column(15).Width = 8;
-                worksheet.Column(16).Width = 8;
-                worksheet.Column(17).Width = 14;
-
-                foreach(var del in theSettings.DEL_COLUMNS.Split(new char [] {','}, StringSplitOptions.RemoveEmptyEntries).OrderByDescending(x=>Convert.ToInt32(x)))
+                foreach (var col in theSettings.COLS_SIZE180PX.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
+                    try
+                    {
+                        worksheet.Column(Convert.ToInt32(col)).Width = (185 - 12) / 7d + 1;
+                    }
+                    catch (Exception ex)
+                    {
+                        Log("COLS_SIZE180PX");
+                        Log(ex.Message);
+                        Log(col);
+                        Log(theSettings.COLS_SIZE180PX);
+                        Log(ex.StackTrace);
+                    }                    
+                }
+
+                foreach (var col in theSettings.COLS_SIZE103PX.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    try
+                    {
+                        worksheet.Column(Convert.ToInt32(col)).Width = (108 - 12) / 7d + 1;
+                    }
+                    catch (Exception ex)
+                    {
+                        Log("COLS_SIZE103PX");
+                        Log(ex.Message);
+                        Log(col);
+                        Log(theSettings.COLS_SIZE103PX);
+                        Log(ex.StackTrace);
+                    }
+                }
+
+
+                foreach (var col in theSettings.COLS_SIZE061PX.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    try
+                    {
+                        worksheet.Column(Convert.ToInt32(col)).Width = (66 - 12) / 7d + 1;
+                    }
+                    catch (Exception ex)
+                    {
+                        Log("COLS_SIZE061PX");
+                        Log(ex.Message);
+                        Log(col);
+                        Log(theSettings.COLS_SIZE061PX);
+                        Log(ex.StackTrace);
+                    }
+                }
+                
+                Log("del_column");
+                Log(theSettings.DEL_COLUMNS);
+                foreach (var del in theSettings.DEL_COLUMNS.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).OrderByDescending(x => Convert.ToInt32(x)))
+                {
+                    var colCnts = worksheet.Dimension.End.Column;
+                    Log(del + " - " + colCnts);
                     worksheet.DeleteColumn(Convert.ToInt32(del));
+                    Log(Convert.ToInt32(del).ToString());
+                    Log("del ok");
                 }
 
                 var rowCnt = worksheet.Dimension.End.Row;
-                var colCnt = worksheet.Dimension.End.Column;
 
                 string fileTxt = theSettings.USERPATH + "\\AppData\\Local\\CSV-to-Excel\\csv-to-excel.txt";
 
@@ -105,16 +200,61 @@ namespace CsvToExcel
                     throw new Exception("Файл с текстовой информацией отсутствует " + fileTxt);
                 }
 
-                string readTxt = File.ReadAllText(fileTxt);
+                Log("COLOR_FILL_TEXT");
+                Log(theSettings.COLOR_FILL_TEXT);
+                foreach (var token in theSettings.COLOR_FILL_TEXT.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Log(token);
+                }
 
-                worksheet.Cells[rowCnt + 2, 1, rowCnt + 2, 1].Value = readTxt;
-
-                worksheet.Cells[2, 3, 11, 18].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
-                worksheet.Cells[2, 3, 11, 18].Style.Fill.BackgroundColor.SetColor(Color.FromArgb(
+                System.Drawing.Color theColor = Color.FromArgb(
                     Convert.ToInt32(theSettings.COLOR_FILL_TEXT.Split(new char[] { '-' }, StringSplitOptions.None)[0]),
                     Convert.ToInt32(theSettings.COLOR_FILL_TEXT.Split(new char[] { '-' }, StringSplitOptions.None)[1]),
-                    Convert.ToInt32(theSettings.COLOR_FILL_TEXT.Split(new char[] { '-' }, StringSplitOptions.None)[2])));
+                    Convert.ToInt32(theSettings.COLOR_FILL_TEXT.Split(new char[] { '-' }, StringSplitOptions.None)[2]));
 
+
+
+                var readTxt = File.ReadAllLines(fileTxt);
+
+                foreach(var line in readTxt)
+                {
+                    Uri uriResult;
+                    bool result = Uri.TryCreate(line, UriKind.Absolute, out uriResult)
+                        && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+                                        
+                    if (result)
+                    {
+                        var er = new ExcelHyperLink(line);
+                        worksheet.Cells[rowCnt + 2, 1, rowCnt + 2, 1].Hyperlink = new Uri(line);
+                        worksheet.Cells[rowCnt + 2, 1, rowCnt + 2, 1].Value = line;
+
+                        try
+                        {
+                            var namedStyle = worksheet.Workbook.Styles.CreateNamedStyle("HyperLink");
+                            namedStyle.Style.Font.UnderLine = true;
+                            namedStyle.Style.Font.Color.SetColor(Color.Blue);
+                        }
+                        catch(Exception ex)
+                        {
+                            Log(ex.Message);
+                        }
+                        
+                        worksheet.Cells[rowCnt + 2, 1, rowCnt + 2, 1].StyleName = "HyperLink";
+                    }
+                    else
+                    {
+                        worksheet.Cells[rowCnt + 2, 1, rowCnt + 2, 1].Value = line;
+                    }
+
+                    worksheet.Cells[rowCnt + 2, 1, rowCnt + 2, 11].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                    worksheet.Cells[rowCnt + 2, 1, rowCnt + 2, 11].Style.Fill.BackgroundColor.SetColor(theColor);
+                    rowCnt++;
+                }
+
+                
+
+
+                
                 package.Save();
             }
         }
--- CsvToExcel/Settings.cs
+++ CsvToExcel/Settings.cs
@@ -10,54 +10,95 @@ namespace CsvToExcel
 {
     public class Settings
     {
+        protected string fileLog;
         public string FILENAME;
         public string DEL_COLUMNS;
         public string COLOR_FILL_HEADER;
         public string COLOR_FILL_TEXT;
         public string PREFIX;
+        public string COLS_SIZE180PX;
+        public string COLS_SIZE061PX;
+        public string COLS_SIZE103PX;
+        public int SIZE_DEFAULT_IN_PX;
+        public string DELIMITER;
+        public int DEBUG;
 
+        public Settings(string fileLog)
+        {
+            this.fileLog = fileLog;
+        }
+        public void Log(string msg)
+        {
+            if (this.DEBUG > 0)
+            {
+                File.AppendAllText(fileLog, msg);
+                File.AppendAllText(fileLog, "\r\n");
+            }            
+        }
         public string USERPATH
         {
             get
             {
                 System.Environment.GetEnvironmentVariable("USERPROFILE");
-
                 string pathUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-
                 return pathUser;
             }
         }
         public void Load()
-        {            
+        {
+            Log("Load");
             System.Environment.GetEnvironmentVariable("USERPROFILE");
 
             string pathUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-
-            //if (Directory.Exists("pathUser") == false )
-            //{
-            //    throw new Exception("Отсутствует каталог пользователя");
-            //}
+            Log("GetFolderPath" );
             if (Directory.Exists(pathUser + @"\AppData\Local\CSV-to-Excel\") == false)
             {
+                Log("CreateDir ");
                 Directory.CreateDirectory(pathUser + @"\AppData\Local\CSV-to-Excel\");
+            }
+            else
+            {
+                Log("Already CreateDir ");
             }
 
             AMS.Profile.Ini Ini = new AMS.Profile.Ini(pathUser + @"\AppData\Local\CSV-to-Excel\csv-to-excel.ini");
 
             if (File.Exists(pathUser + @"\AppData\Local\CSV-to-Excel\csv-to-excel.ini") == false)
             {
+                Log("CreateLogDir set " );
                 Ini.SetValue("COMMON", "FILENAME", "batch_analysis_export.utf-16.csv");
-                Ini.SetValue("COMMON", "DEL_COLUMNS", "1,2,3");
+                Ini.SetValue("COMMON", "DEL_COLUMNS", "1,3,5,6,7,8");
                 Ini.SetValue("COMMON", "COLOR_FILL_HEADER", "200-250-200");
-                Ini.SetValue("COMMON", "COLOR_FILL_TEXT", "200-240-200");
+                Ini.SetValue("COMMON", "COLOR_FILL_TEXT", "255-242-204");
                 Ini.SetValue("COMMON", "PREFIX", "otchet_summ_");
+                Ini.SetValue("COMMON", "COLS_SIZE180PX", "2");
+                Ini.SetValue("COMMON", "COLS_SIZE103PX", "4,17");
+                Ini.SetValue("COMMON", "COLS_SIZE061PX", "9,10,11,12,13,14,15,16");
+                Ini.SetValue("COMMON", "SIZE_DEFAULT_IN_PX", 72);
+                Ini.SetValue("COMMON", "DELIMITER", "'\t'");
+                Ini.SetValue("COMMON", "DEBUG", 0);
+            }
+            else
+            {
+                Log("Alraedy CreateLogDir ");
             }
             this.FILENAME = Ini.GetValue("COMMON", "FILENAME", "sample.csv");
-            this.DEL_COLUMNS = Ini.GetValue("COMMON", "DEL_COLUMNS", "1,2,3");
+            this.DEL_COLUMNS = Ini.GetValue("COMMON", "DEL_COLUMNS", "1,3,5,6,7,8");
             this.COLOR_FILL_HEADER = Ini.GetValue("COMMON", "COLOR_FILL_HEADER", "200-250-200");
-            this.COLOR_FILL_TEXT = Ini.GetValue("COMMON", "COLOR_FILL_TEXT", "200-240-200");
+            this.COLOR_FILL_TEXT = Ini.GetValue("COMMON", "COLOR_FILL_TEXT", "255-242-204");
             this.PREFIX = Ini.GetValue("COMMON", "PREFIX", "otchet_summ_");
+            this.COLS_SIZE180PX = Ini.GetValue("COMMON", "COLS_SIZE180PX", "2");
+            this.COLS_SIZE103PX = Ini.GetValue("COMMON", "COLS_SIZE103PX", "4,17");
+            this.COLS_SIZE061PX = Ini.GetValue("COMMON", "COLS_SIZE061PX", "9,10,11,12,13,14,15,16");
+            this.SIZE_DEFAULT_IN_PX = Ini.GetValue("COMMON", "SIZE_DEFAULT_IN_PX", 72);
+            this.DELIMITER = Ini.GetValue("COMMON", "DELIMITER", "'\t'");
+            this.DEBUG = Ini.GetValue("COMMON", "DEBUG", 0);
 
+            Log(this.FILENAME);
+            Log(this.DEL_COLUMNS);
+            Log(this.COLOR_FILL_HEADER);
+            Log(this.COLOR_FILL_TEXT);
+            Log(this.PREFIX);
         }
 
     }
